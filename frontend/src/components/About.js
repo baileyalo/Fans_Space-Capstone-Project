48e9a40d1b1e6ecb57e393ee43b54ed0b531ed71
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-04-09 18:55:00 -0400

    update frontend

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 
 class About extends Component {
+
+
   render() {
+
+    const styleObj = {
+      fontSize: 18,
+      color: "black",
+      textAlign: "center",
+      paddingTop: "100px",
+      marginLeft: .5
+    
+    }
+    const head = {
+      fontSize:40 ,
+      color: "Green",
+      textAlign: "left",
+      paddingTop: "5px",
+      font:"bold"    
+          
+    }
     return (
         <div>
-        <h2 style={{color: "Green"}}>About</h2>
-          <div style={{paddingTop: '2em'}}>
-      		<p>Eh up, me duck!</p>
+        <h2 style={head}>About</h2>
+          <div>
+            <div className='text'>
+      		<pre style={styleObj}> Fans Space is a Decentralized Application Built on Ethereum Blockchain network<br/>
+          <br/>
+            that is designed to allows Fans from sporting events to be able to purchase tokens that <br/>
+            <br/>
+            are made available by the sports teams. These tokens have a infinite lifetime and allows<br/>
+            <br/> 
+            fans to be able to make decision on their teams through voting polls, VIP access to teams<br/>
+            <br/>
+            unlimited perks for global fans and can also be traded with other token holders.
+          </pre>
+          </div>
       	</div>
         </div>
     );
