e19353c0a79621b074b57e59e09a670c51daf90c
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-04-13 19:08:06 -0400

    update

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
-const Storage = artifacts.require("./Storage.sol");
+const KycContract = artifacts.require("./KycContract.sol");
+const Crowdsale = artifacts.require("./Crowdsale.sol");
 
 module.exports = function (deployer) {
- // deployer.deploy(Storage);
+  deployer.deploy(KycContract);
+  deployer.deploy(Crowdsale);
 };
--- migrations/3_token_contract.js
+++ migrations/3_token_contract.js
@@ -1,5 +1,9 @@
 const TokenGenerator = artifacts.require("./TokenGenerator.sol");
+const FANTOKEN = artifacts.require("./FANTOKEN.sol");
+const FanTokenSale = artifacts.require("./FanTokenSale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(TokenGenerator);
+  deployer.deploy(FANTOKEN);
+  deployer.deploy(FanTokenSale);
 };
