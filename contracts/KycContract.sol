09c488a3c49304fa664cfaf7e5f16f8171c87a58
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-04-13 17:22:12 -0400

    update contracts

---
+++ contracts/KycContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract KycContract is Ownable {
+    mapping(address => bool) allowed;
+
+    function setKycCompleted(address _addr) public onlyOwner {
+        allowed[_addr] = true;
+    }
+
+    function setKycRevoked(address _addr) public onlyOwner {
+        allowed[_addr] = false;
+    }
+
+    function kycCompleted(address _addr) public view returns (bool) {
+        return allowed[_addr];
+    }
+
+    function getOwner () public view returns(address) {
+        return owner(); 
+    }
+}
--- test/FANTOKEN.test.js
+++ test/FANTOKEN.test.js
@@ -1,4 +1,4 @@
-// require("dotenv").config({ path: "../.env" });
+ require("dotenv").config({ path: "../.env" });
 
 const Token = artifacts.require("FANTOKEN");
 
